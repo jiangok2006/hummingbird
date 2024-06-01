ddfc359fb4aa1631d77ccd02bb0727bb1483d90a
jiangok-hs <lian.jiang@highspot.com>
2024-06-01 14:46:09 -0700

    configure argo project

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -7,3 +7,17 @@ function create_argo_project() {
     argocd appset list | grep hummingbird-jiangok
 }
 
+function reinstall_hummingbird() {
+    argocd app delete hummingbird-jiangok
+    argocd appset delete hummingbird-jiangok
+    argocd proj delete hummingbird-jiangok
+
+    # create argo project
+    argocd proj create hummingbird-jiangok ~/repo/hummingbird_jiangok/argocd/project.yml
+
+    # configure argo project
+    k apply -f ~/repo/hummingbird_jiangok/argocd/project.yml
+
+    # confirm repositories are set.
+    argocd proj get hummingbird-jiangok 
+}
